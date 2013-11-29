7a2026ab08f2a896a8b518f0d1ce431bb9d8821e
Goldenwing <kevin.tanguay@gmail.com>
2013-11-29 12:59:57 -0500

    Update Game.java

--- Battleships-TP3/src/battleships/backend/Game.java
+++ Battleships-TP3/src/battleships/backend/Game.java
@@ -1,5 +1,7 @@
 package battleships.backend;
 
+import java.util.Random;
+
 public class Game 
 {
 
@@ -24,7 +26,84 @@ public class Game
 		}
 	}
 
-	private static final int   GAME_SIZE = 10;
+	private final int GAME_SIZE = 10;
+	private Matrix playerMatrix;
+	private Matrix computerMatrix;
+	private Random PCBoatPlacer = new Random();
+	
+	public Game()
+	{
+		this.playerMatrix = null;
+		this.computerMatrix = null;
+		
+		this.playerMatrix = new Matrix();
+		this.computerMatrix = new Matrix(true);
+		
+		ComputerPlayer enemy = new ComputerPlayer("Ganondorf");
+		HumanPlayer gamer = new HumanPlayer("Link");
+		
+		enemy.setBoats(this.computerMatrix, PCBoatPlacer.nextBoolean(), Boats.AIRCRAFT);
+		enemy.setBoats(this.computerMatrix, PCBoatPlacer.nextBoolean(), Boats.BATTLESHIP);
+		enemy.setBoats(this.computerMatrix, PCBoatPlacer.nextBoolean(), Boats.DESTROYER);
+		enemy.setBoats(this.computerMatrix, PCBoatPlacer.nextBoolean(), Boats.SUBMARINE);
+		enemy.setBoats(this.computerMatrix, PCBoatPlacer.nextBoolean(), Boats.PATROL);
+	}
+	
+	 public Matrix getMatrix()
+	 {
+		 return this.playerMatrix;
+	 }
+	 
+	 public Matrix getComputerMatrix()
+	 {
+		 return this.computerMatrix;
+	 }
+	 
+	public void setComputerShips(Boats boat, boolean direction)
+	{
+		int randomX = PCBoatPlacer.nextInt(this.GAME_SIZE -1);
+		int randomY = PCBoatPlacer.nextInt(this.GAME_SIZE -1);
+		
+		while(!this.computerMatrix.checkSpace(boat, direction, randomX, randomY))
+		{
+			randomX = PCBoatPlacer.nextInt(this.GAME_SIZE -1);
+			randomY = PCBoatPlacer.nextInt(this.GAME_SIZE -1);
+		}
+
+		if(direction) //si horizontal
+		{
+			for(int x = randomX; x < randomX + boat.getSize(); x++)
+			{
+				this.computerMatrix.setSquareContent(x, randomY, boat.getSize(), true);
+			}
+		}
+		else // si vertical
+		{
+			for(int y = randomY; y < randomY + boat.getSize(); y++)
+			{
+					this.computerMatrix.setSquareContent(randomX, y, boat.getSize(), true);
+			}
+		}
+	}
 	
 	
+	public void setPlayerShips(String boat, int size, boolean direction)
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		Game game = new Game();
+		Matrix computerMatrix = game.getComputerMatrix();
+		for(int i = 0; i < 10; i++)
+		   {
+		      for(int j = 0; j < 10; j++)
+		      {
+		         System.out.printf("%5d ", computerMatrix.getSquareContentNumber(i, j, true));
+		      }
+		      System.out.println();
+		   }
+	}
 }
