6acf5935a8dfbd9820c073ffc2dfcda44f07e35b
Laurie Lavoie <laurie13avril@hotmail.com>
2013-11-29 13:13:38 -0500

    Create EnemyGridGame.java

---
+++ Battleships-TP3/src/battleships/EnemyGridGame.java
@@ -0,0 +1,60 @@
+package battleships;
+
+import javafx.scene.Group;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+public class EnemyGridGame 
+{
+	public EnemyGridGame()
+	
+	{
+		
+	}
+	
+	
+	public Group setGrid()
+	{
+		Group rootEnemy = new Group();
+		
+		Text name = new Text("Ordinateur");
+		name.setX(385);
+		name.setY(870);
+		name.setFont(new Font(20));
+		int nbSquare = 11;
+		
+		int xSquare = 50; // coordonnees x de l'image
+		int ySquare = 70; // coordonnees y de l'image
+				
+		for(int i = 0; i < nbSquare; i++)
+		{
+		
+			if( i > 0)
+			{
+				xSquare += 70;
+				ySquare = 70;
+			}
+			for(int j = 0; j <  nbSquare; j++)
+			{
+				
+				if(j > 0)
+				{
+					ySquare += 70;
+				}
+				ImageView waterTiles = new ImageView(new Image("file:Images/water.png"));
+				waterTiles.setX(xSquare);
+				waterTiles.setY(ySquare);
+				rootEnemy.getChildren().add(waterTiles);
+			
+			}
+		}
+		
+		rootEnemy.getChildren().add(name);
+		
+		return rootEnemy;
+		
+	}
+}
