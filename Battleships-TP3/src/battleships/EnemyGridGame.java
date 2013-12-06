ffcb52a7712155de19de01c47cac14f1465eeb82
Goldenwing <kevin.tanguay@gmail.com>
2013-12-06 15:30:34 -0500

    Update EnemyGridGame.java

--- Battleships-TP3/src/battleships/EnemyGridGame.java
+++ Battleships-TP3/src/battleships/EnemyGridGame.java
@@ -2,6 +2,9 @@ package battleships;
 
 import java.util.List;
 
+import battleships.backend.Game;
+import battleships.backend.Matrix;
+
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.image.Image;
@@ -12,85 +15,88 @@ import javafx.scene.text.Text;
 
 public class EnemyGridGame
 {
+	private ImageView[][] imageViewTab;
+	Game game;
         
+    public EnemyGridGame(Game game)
+    {
+    	this.game = game;
+        this.imageViewTab = new ImageView[11][11];        
+    }
         
+    public ImageView[][] getImageTab()
+    {
+    	return this.imageViewTab;
+    }
         
-		private ImageView[][] imageViewTab;
+     public Group setGrid()
+     {
+        Group rootEnemy = new Group();
+                
+        Text name = new Text("Ordinateur");
+        name.setX(385);
+        name.setY(870);
+        name.setFont(new Font(20));
+        int nbSquare = 11;
+        
+        int xSquare = 50; // coordonnees x de l'image
+        int ySquare = 70; // coordonnees y de l'image
+        ImageList imageList =  new ImageList();
+        List<Image> imageListNumber = imageList.imageListNumbers();
+        List<Image> imageListLetter = imageList.imageListLetters();
+        
+                                
+        for(int i = 0; i < nbSquare; i++)
+        {
+        	if( i > 0)
+            {
+	              xSquare += 70;
+	              ySquare = 70;
+            }
+        	
+        	for(int j = 0; j < nbSquare; j++)
+	        {
+	        	ImageView waterTiles = new ImageView(new Image("file:Images/water.png"));
+	          
+	        	if(j > 0 && i > 0)
+	        	{
+	               ySquare += 70;
+	               waterTiles = new ImageView(new Image("file:Images/water.png"));
+	            }
+	        	
+	            else if(j == 0 && i > 0)
+	            {
+	               waterTiles = new ImageView(imageListNumber.get(i - 1));
+	            }
+	        	
+	            else if(j > 0 && i == 0)
+	            {
+	               ySquare += 70;
+	               waterTiles = new ImageView(imageListLetter.get(j - 1));
+	            }
 	
-        
-        public EnemyGridGame()
-        
-        {
-        	this.imageViewTab = new ImageView[11][11];	
+	            this.imageViewTab[i][j] = waterTiles;
+	            waterTiles.setX(xSquare);
+	            waterTiles.setY(ySquare);
+	            rootEnemy.getChildren().add(waterTiles);
+	         }
         }
         
-    	
-    	public ImageView[][] getImageTabViewMyGrid()
-    	{
-    		return this.imageViewTab;
-    	}
-        
-        public Group setGrid()
+        for(int i = 1; i < nbSquare; i++)
         {
-                Group rootEnemy = new Group();
-                
-                Text name = new Text("Ordinateur");
-                name.setX(385);
-                name.setY(870);
-                name.setFont(new Font(20));
-                int nbSquare = 11;
-        
-                int xSquare = 50; // coordonnees x de l'image
-                int ySquare = 70; // coordonnees y de l'image
-                ImageList imageList =  new ImageList();
-                List<Image> imageListNumber = imageList.imageListNumbers();
-                List<Image> imageListLetter = imageList.imageListLetters();
-                                
-                for(int i = 0; i < nbSquare; i++)
-                {
-                
-                        if( i > 0)
-                        {
-                                xSquare += 70;
-                                ySquare = 70;
-                        }
-                        for(int j = 0; j < nbSquare; j++)
-                        {
-                        	
-                            	ImageView waterTiles = new ImageView(new Image("file:Images/water.png"));
-                                
-                                if(j > 0 && i > 0)
-                                {
-                                        ySquare += 70;
-                                        waterTiles = new ImageView(new Image("file:Images/water.png"));
-                                }
-                                else if(j == 0 && i > 0)
-                                {
-                                	 waterTiles = new ImageView(imageListNumber.get(i - 1));
-                                }
-                                
-                                else if(j > 0 && i == 0)
-                                {
-                                	 ySquare += 70;
-                                	 waterTiles = new ImageView(imageListLetter.get(j - 1));
-                                }
-
-                        		this.imageViewTab[i][j] = waterTiles;
-                                waterTiles.setX(xSquare);
-                                waterTiles.setY(ySquare);
-                                waterTiles.setOnMouseClicked(new MouseListener());
-                                rootEnemy.getChildren().add(waterTiles);
-                        
-                        }
-                }
-                
-                rootEnemy.getChildren().add(name);
-                
-                return rootEnemy;
-                
+        	for(int j = 1; j < nbSquare; j++)
+        	{
+        		 this.imageViewTab[i][j].setOnMouseClicked(new MouseListener());
+        	}
         }
+       
         
-        private class MouseListener implements EventHandler<MouseEvent>
+         
+        rootEnemy.getChildren().add(name);
+        return rootEnemy;      
+     }
+	
+	private class MouseListener implements EventHandler<MouseEvent>
 	{
 
 		@Override
@@ -165,3 +171,4 @@ public class EnemyGridGame
 		return y;
 	}
 }
+
