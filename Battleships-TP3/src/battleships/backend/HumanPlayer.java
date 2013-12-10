39dfbe7af87714cb632d60c8086813ac77494e90
Laurie Lavoie <laurie13avril@hotmail.com>
2013-12-10 16:51:26 -0500

    Human player qui bug et pas fini

--- Battleships-TP3/src/battleships/backend/HumanPlayer.java
+++ Battleships-TP3/src/battleships/backend/HumanPlayer.java
@@ -4,28 +4,26 @@ import battleships.backend.Game.Boats;
 
 
 /**
- * Extension de la super-classe Player qui permet d'instancier un joueur humain (joueur interagissant
- * avec l'interface graphique pour jouer)
- * @author Annie Belzile
- *
- */
+* Extension de la super-classe Player qui permet d'instancier un joueur humain (joueur interagissant
+* avec l'interface graphique pour jouer)
+* @author Annie Belzile
+*
+*/
 public class HumanPlayer extends Player {
         
         private String playerName;
         
-        public HumanPlayer(String name) {
+        public HumanPlayer(String name)
+        {
                 this.playerName = name;
         }
 
-        @Override
-		public void setBoats(Matrix gameGrid, boolean direction, Boats boat) {
-			// TODO Auto-generated method stub
-			
-		}
+       
+ 
         
         @Override
         public boolean shootEnemy(Matrix gameGrid) {
-			return false;
+                        return false;
                 // TODO Auto-generated method stub
                 
         }
@@ -37,4 +35,55 @@ public class HumanPlayer extends Player {
         public void setPlayerName(String name) {
                 this.playerName = name;
         }
+
+
+		
+		public void setBoats(Matrix gameGrid, int verifX, int verifY, boolean direction, Boats boat)
+		
+		{
+			
+			int randomX = 0;
+			int randomY = 0;
+			
+		
+			
+			 while(randomX == 0 || randomY == 0 ||!gameGrid.checkSpace(boat, direction, randomX, randomY))
+	            {
+	            	randomX++;
+	                randomY++;
+	            }
+	            
+        	
+//        	if(direction) 
+//            { //si horizontal
+//        		
+//                for(int x =verifX; x <verifX + boat.getSize(); x++) 
+//                {
+//                    gameGrid.setSquareContent(x, verifY, boat, true, boat.getBoatName());   
+//                }
+//            } 
+//               
+//        	else 
+//               
+//        	{ // si vertical
+//                      
+//        		for(int y = verifY; y < verifY + boat.getSize(); y++) 
+//                      
+//        		{      
+//                    gameGrid.setSquareContent(verifX, y, boat, true, boat.getBoatName());
+//                      
+//        		}
+//              
+//        	}  
+			
+		}
+
+
+
+
+		@Override
+		public void setBoats(Matrix gameGrid, boolean direction, Boats boat) {
+			// TODO Auto-generated method stub
+			
+		}
 }
