2f43bef5cffe8c3be05e25f2a236acfdcd056aad
Laurie Lavoie <laurie13avril@hotmail.com>
2013-12-10 15:24:20 -0500

    Update BattleGame.java

--- Battleships-TP3/src/battleships/BattleGame.java
+++ Battleships-TP3/src/battleships/BattleGame.java
@@ -1,13 +1,13 @@
 package battleships;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 import battleships.backend.Game;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -15,6 +15,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.RadioButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
@@ -30,7 +31,9 @@ public class BattleGame extends Application
 	private ModalWindow modal;
 	private String name;
 	private ImageView[] tabImage;
-	private Game game;
+	public Game game;
+	int [][] tableXY;
+	public List<RadioButton> listRbuttonChecked;
 	
 	public void start(Stage stage)
 	{
@@ -38,11 +41,10 @@ public class BattleGame extends Application
 		Scene scene = new Scene(this.root, 1920, 1000, Color.LIGHTGRAY);
 		
 		this.game = new Game();
-		
+//		
 		GameMenu gameMenu = new GameMenu(game);
 		this.root.getChildren().add(gameMenu);
-		
-		stage.setTitle("BattleShips  (Annie Belzile, Laurie Lavoie, Kevin Tanguay)"); 
+		stage.setTitle("Jeu de simulation de combat en territoire navale.  (Annie Belzile, Laurie Lavoie, Kevin Tanguay)"); 
         stage.setScene(scene); 
        
 //        stage.setResizable(false);
@@ -52,6 +54,7 @@ public class BattleGame extends Application
 	
 	}
 	
+	
 	public void askPositionBoats()
 	{
 		
@@ -59,7 +62,7 @@ public class BattleGame extends Application
 		this.modal = new ModalWindow();
 		gridPosition = modal.setItemModal();
 		this.stagePosition = new Stage();
-	
+		this.tableXY  = new int[2][5];
 		Button okBtn = new Button("Ok");
 		okBtn.setOnAction(new ButtonListener());
 		gridPosition.add(okBtn, 1, 7);
@@ -69,13 +72,14 @@ public class BattleGame extends Application
         this.stagePosition.setScene(scenePosition);
         this.stagePosition.show();
 	}
+
 	
-	public void setGrids(Game game)
+	public void setGrids(Game game2)
 	{	
 		this.setPieces();
 		MyGameGrid myGrid = new MyGameGrid();
 		
-		EnemyGridGame enemyGrid = new EnemyGridGame(game);
+		EnemyGridGame enemyGrid = new EnemyGridGame();
 		this.root.getChildren().add(enemyGrid.setGrid());
 		this.root.getChildren().add(myGrid.setGrid(this.name));
 	}
@@ -98,6 +102,14 @@ public class BattleGame extends Application
 	{
 		
 	}
+	
+	public boolean verifiedSettingBoats()
+	{
+//		boolean verified = this.game.checkBoatsUser(this.tableXY, this.listRbuttonChecked);
+		boolean verified = this.game.checkBoatsUser(this.tableXY, this.listRbuttonChecked);
+		return verified;
+	}
+	
 	public static void main(String[] args)
 	{
 //		JUnitCore junit = new JUnitCore(); 
@@ -138,13 +150,14 @@ public class BattleGame extends Application
 	private class ButtonListener implements EventHandler<ActionEvent>
 	{
 		
-		int [][] tableXY  = new int[2][5];
+
 		
 		@Override
 		public void handle(ActionEvent arg0)
 		{
 			boolean verified = true;
 			int compteur = 1;
+			BattleGame.this.listRbuttonChecked = new ArrayList<RadioButton>();
 			
 			BattleGame.this.modal.getErrorText().setVisible(false);
 			if(BattleGame.this.modal.getListField().get(0).getText().isEmpty())
@@ -163,12 +176,32 @@ public class BattleGame extends Application
 				for(int i = 0; i < 2; i++)
 				{
 					int textLenght = BattleGame.this.modal.getListField().get(compteur).getText().length();
-					if(textLenght == 1 )
+					if(textLenght == 1 || textLenght == 2)
 					{
 						char checkIfnumber = BattleGame.this.modal.getListField().get(compteur).getText().charAt(0);
-						if(checkIfnumber > 47 && checkIfnumber < 58)
+						boolean checkSecondChar = true;
+						if(textLenght == 2)
 						{
-							this.tableXY[i][j] = Integer.parseInt(BattleGame.this.modal.getListField().get(compteur).getText());
+							char checkIfSecondNumber = BattleGame.this.modal.getListField().get(compteur).getText().charAt(1);
+							if(checkIfSecondNumber == 48)
+							{
+								checkSecondChar = true;
+							}
+							else
+							{
+								checkSecondChar = false;
+							}
+						}
+						
+						if(checkIfnumber > 47 && checkIfnumber < 58 && checkSecondChar)
+						{
+							
+							BattleGame.this.tableXY[i][j] = Integer.parseInt(BattleGame.this.modal.getListField().get(compteur).getText());
+							boolean checked = BattleGame.this.modal.getListRadioButton().get(compteur - 1).isSelected();
+							if(checked == true)
+							{
+								BattleGame.this.listRbuttonChecked.add(BattleGame.this.modal.getListRadioButton().get(compteur - 1));
+							}
 							compteur++;
 							
 				;
@@ -189,8 +222,18 @@ public class BattleGame extends Application
 			
 			}
 			
+			if(!BattleGame.this.verifiedSettingBoats())
+			{
+				BattleGame.this.modal.getErrorText().setVisible(true);
+				verified = false;
+				
+			}
+			
+			
 			if(verified == true)
 			{
+				
+				
 				 BattleGame.this.stagePosition.close();
 				 BattleGame.this.modal.getErrorText().setVisible(false);
 				
