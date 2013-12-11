68896570905b79d22bdc69282f2beee836e25a52
Laurie Lavoie <laurie13avril@hotmail.com>
2013-12-11 15:06:17 -0500

    Update HumanPlayer.java

--- Battleships-TP3/src/battleships/backend/HumanPlayer.java
+++ Battleships-TP3/src/battleships/backend/HumanPlayer.java
@@ -38,44 +38,63 @@ public class HumanPlayer extends Player {
 
 
 		
-		public void setBoats(Matrix gameGrid, int verifX, int verifY, boolean direction, Boats boat)
+		public boolean setBoats(Matrix gameGrid, int verifX, int verifY, boolean direction, Boats boat)
 		
 		{
 			
-			int randomX = 0;
-			int randomY = 0;
+			int randomX = 1;
+			int randomY = 1;
+			boolean valid = true;
+			int size = boat.getSize();
 			
-		
+			 
+			if(gameGrid.checkSpace(boat, direction, verifX, verifY, gameGrid.getGameMatrix()) && (randomX <= size && randomY <= size) && valid == true)
+			{
+				randomX++;
+                randomY++;
+				valid = true;
+			}
+			else
+			{
+				valid = false;
+			}
 			
-			 while(randomX == 0 || randomY == 0 ||!gameGrid.checkSpace(boat, direction, randomX, randomY))
-	            {
-	            	randomX++;
-	                randomY++;
-	            }
+			
+//			 while(randomX == 0 || randomY == 0 ||!gameGrid.checkSpace(boat, direction, randomX, randomY, gameGrid.getGameMatrix()))
+//	            {
+//	            	randomX++;
+//	                randomY++;
+//	            }
 	            
+        	if(valid)
+        	{
+        		
         	
-//        	if(direction) 
-//            { //si horizontal
-//        		
-//                for(int x =verifX; x <verifX + boat.getSize(); x++) 
-//                {
-//                    gameGrid.setSquareContent(x, verifY, boat, true, boat.getBoatName());   
-//                }
-//            } 
-//               
-//        	else 
-//               
-//        	{ // si vertical
-//                      
-//        		for(int y = verifY; y < verifY + boat.getSize(); y++) 
-//                      
-//        		{      
-//                    gameGrid.setSquareContent(verifX, y, boat, true, boat.getBoatName());
-//                      
-//        		}
-//              
-//        	}  
+	        	if(direction) 
+	            { //si horizontal
+	        		
+	                for(int x =verifX; x <verifX + boat.getSize(); x++) 
+	                {
+	                    gameGrid.setSquareContent(x, verifY, boat, false, boat.getBoatName());   
+	                }
+	            } 
+	               
+	        	else 
+	               
+	        	{ // si vertical
+	                      
+	        		for(int y = verifY; y < verifY + boat.getSize(); y++) 
+	                      
+	        		{      
+	                    gameGrid.setSquareContent(verifX, y, boat, false, boat.getBoatName());
+	                      
+	        		}
+	              
+	        	}  
 			
+        	}
+        	
+        	return valid;
 		}
 
 
