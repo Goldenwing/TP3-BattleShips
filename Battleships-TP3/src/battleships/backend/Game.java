a4987b8437a2ff7d33a620a7a7e50385d163145b
Annie B. <Anikuni@users.noreply.github.com>
2013-12-13 09:22:21 -0500

    Update Game.java
    enlever les import javafx inutiles

--- Battleships-TP3/src/battleships/backend/Game.java
+++ Battleships-TP3/src/battleships/backend/Game.java
@@ -15,17 +15,8 @@ import java.util.Random;
 
 import battleships.BattleGame;
 
-import javafx.application.Platform;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
+
 
 public class Game
 {
