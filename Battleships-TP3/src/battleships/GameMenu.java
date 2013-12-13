9490c30a43cf78a64240fb8a9c75cefbca3ab2aa
Laurie Lavoie <laurie13avril@hotmail.com>
2013-12-13 17:10:17 -0500

    Update GameMenu.java

--- Battleships-TP3/src/battleships/GameMenu.java
+++ Battleships-TP3/src/battleships/GameMenu.java
@@ -1,10 +1,26 @@
 package battleships;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
 import battleships.backend.Game;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -18,7 +34,10 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -172,43 +191,161 @@ public class GameMenu extends Parent
 	 */
 	private class menuListenerSeeScores implements EventHandler<ActionEvent>
 	{
+		
+		private GridPane scoreRoot;
 		Stage stage;
 		@Override
 		public void handle(ActionEvent arg0)
 		{
-			Group scoreRoot = new Group();
+			scoreRoot = new GridPane();
+			 scoreRoot.setAlignment(Pos.CENTER);
+		        scoreRoot.setHgap(10);
+		        scoreRoot.setVgap(10);
+		        scoreRoot.setPadding(new Insets(25, 25, 25, 25));
 			Stage scoreDialog = new Stage();
 			scoreDialog.initModality(Modality.APPLICATION_MODAL);
-			Scene scoreScene = new Scene(scoreRoot, 200,100, Color.LIGHTGRAY);
-			
+			Scene scoreScene = new Scene(this.scoreRoot, 200,100, Color.LIGHTGRAY);
+			int line = takeScoresInFile();
 			Button buttonErase = new Button("Effacer les scores");
-				buttonErase.setLayoutX(25);
-				buttonErase.setLayoutY(75);
+			buttonErase.setOnAction(new ButtonEraseListener());
+			buttonErase.setLayoutX(25);
+			buttonErase.setLayoutY(75);
 			Button buttonAccept = new Button("Ok");
 				buttonAccept.setLayoutX(150);
 				buttonAccept.setLayoutY(75);
 				ButtonOKListener okListener = new ButtonOKListener();
 				buttonAccept.setOnMouseClicked(okListener);
 				
-				scoreRoot.getChildren().add(buttonErase);
-				scoreRoot.getChildren().add(buttonAccept);
+				scoreRoot.add(buttonErase, 0, line);
+				scoreRoot.add(buttonAccept, 1, line);
 			
 			scoreDialog.setTitle("Les demineurs les plus rapides");
 			scoreDialog.setScene(scoreScene);
 			this.stage = scoreDialog;
 			scoreDialog.setResizable(false);
 	        scoreDialog.show(); ;
-		    	scoreDialog.show();
+		    scoreDialog.show();
 			
 		}
 		
-		
+		private int takeScoresInFile()
+		{
+			 
+	    	 String name = null;
+	         String score = "";
+	         String lineScore = "";
+	         int nbLined = 0;
+	         
+	         Document doc = null;
+	      
+	         
+	         try
+	         {    
+	            BufferedReader buff = new BufferedReader(new FileReader("bestScores.txt"));
+	                
+	            try
+	            {
+	            	
+	            	lineScore = buff.readLine();
+	            	while (lineScore != null)
+	            	{	
+	            		
+	            		
+	            		   XPathFactory xpathFactory = XPathFactory.newInstance();
+	            	         XPath xpath = xpathFactory.newXPath();
+	            		
+	            		InputSource source = new InputSource(new StringReader(lineScore));
+	            		
+	            		
+	            		 try
+	                     {
+	                             doc = (Document) xpath.evaluate("/", source, XPathConstants.NODE);
+	                     }
+	                     catch (XPathExpressionException e)
+	                     {        
+	                             e.printStackTrace();
+	                     }
+	                     
+////	                    
+	                    try
+	                     {
+	                         score = xpath.evaluate("/game/nbShots", doc);
+	                         
+	                                
+	                     }
+	                    catch (XPathExpressionException e)
+	                    {
+	                    		 e.printStackTrace();
+	                    }
+	                    
+	                    try
+	                     {
+	                         name = xpath.evaluate("/game/name", doc);
+	                         
+	                                
+	                     }
+	                    catch (XPathExpressionException e)
+	                    {
+	                    		 e.printStackTrace();
+	                    }
+	                    
+	                    System.out.println(name);
+	                    System.out.println(score);
+	                    
+	                    
+	                    Label textName = new Label(name);
+	                    Label textScore = new Label(score);
+	                    
+	                    
+	                    
+	                    this.scoreRoot.add(textName, 0, nbLined);
+	                    this.scoreRoot.add(textScore, 1, nbLined);
+	                    
+	           			nbLined++;
+	                    lineScore = buff.readLine();
+//	                    
+	            	}
+	            	
+	            	
+	            	buff.close();
+	            } 
+	            finally
+	            {
+	            	
+	            }
+	                
+	         }
+	         catch (IOException ioe)
+	         {
+	        	
+	        	
+	         }
+	         
+	         return nbLined;
+	           
+		}
 		
 		private class ButtonOKListener implements EventHandler<MouseEvent>
 		{
 			@Override
 			public void handle(MouseEvent arg0)
 			{
+				menuListenerSeeScores.this.stage.close();
+			}
+		}
+		
+		private class ButtonEraseListener implements  EventHandler<ActionEvent>
+		{
+			@Override
+			public void handle(ActionEvent arg0)
+			{
+				
+				File file = new File("bestScores.txt");
+				if(file.isFile())
+				{
+					file.delete();
+				}
+				
 				menuListenerSeeScores.this.stage.close();
 			}
 		}
@@ -239,7 +376,7 @@ public class GameMenu extends Parent
 				imageView.setLayoutY(60);
 			proposRoot.getChildren().add(imageView);
 
-			Label infoLabel = new Label("Cette application a  ete realisee par \n Annie Belzile, Laurie Lavoie et Kevin Tanguay \n dans le cadre du cours de Programmation III!");
+			Label infoLabel = new Label("Cette application a  ete realisee par \n Annie Belzile, Laurie Lavoie et Kevin Tanguay \n dans le cadre du cours de Programmation III!");
 
 				infoLabel.setLayoutY(5);
 			proposRoot.getChildren().add(infoLabel);
