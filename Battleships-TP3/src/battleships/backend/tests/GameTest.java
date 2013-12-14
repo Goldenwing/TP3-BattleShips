2f98cfd0adeb98da1642595e76ad8ff9bed6429f
Laurie Lavoie <laurie13avril@hotmail.com>
2013-12-13 22:20:54 -0500

    Update GameTest.java

--- Battleships-TP3/src/battleships/backend/tests/GameTest.java
+++ Battleships-TP3/src/battleships/backend/tests/GameTest.java
@@ -4,12 +4,14 @@ import org.junit.Test;
 import org.junit.Assert;
 
 import battleships.backend.Game;
+import battleships.backend.HumanPlayer;
+
 
 public class GameTest 
 {
 
 	@Test
-	public void testConstructor()
+	public void testConstructorStart()
 	{
 		Game test = new Game();
 		
@@ -18,10 +20,36 @@ public class GameTest
 	}
 	
 	@Test
-	public void testSetPlayerBoats()
+	public void testSetGamer()
 	{
 		Game test = new Game();
-		
-		//test.getGamer().setBoats();
+		HumanPlayer gamer = new HumanPlayer("Pikachu");
+		test.setGamer(gamer);
+		Assert.assertNotNull(test.getGamer());
+	
 	}
+	
+	//le test de SetEnemy est exactement le mÃªme test, je ne le rÃ©pÃ¨tera pas.
+	
+	@Test
+	public void testGetGamer()
+	{
+		Game test = new Game();
+		HumanPlayer gamer = new HumanPlayer("Pikachu");
+		test.setGamer(gamer);
+		Assert.assertEquals(gamer, test.getGamer() );
+	
+	}
+	
+	//le test de GetEnemy est exactement le mÃªme test, je ne le rÃ©pÃ¨tera pas.
+	
+	@Test
+	public void testDidWeWin()
+	{
+		Game test = new Game();
+	
+		Assert.assertFalse(test.DidWeWin(test.getMatrix()));
+	
+	}
+	
 }
